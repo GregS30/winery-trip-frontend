91153ce457c75a92ebb7ec0e07622cb5ce704e5c
GregS30 <gjschmitt@live.com>
2018-07-30 18:05:05 -0400

    trips! and login/logout

---
+++ src/components/Login.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  state = {
+    username: "",
+    password: "",
+  }
+
+  // componentDidMount() {
+  //   console.log(localStorage.getItem('token'));
+  // }
+  //
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  // handleSubmit = (event) => {
+  //   event.preventDefault();
+  //
+
+    // event.preventDefault();
+    // fetch('http://localhost:3000/sessions', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(this.state)
+    // })
+    //   .then(res => res.json())
+    //   .then(json => {
+    //     console.log(json)
+    //     // lift state to the top and keep token there
+    //     // refresh == token gone
+    //     // magical place in browser <=====
+    //     // cookie!
+    //     // global variable outside of react == var
+    //     localStorage.setItem('token', json.token);
+    //     this.props.setUser(json.id, json.username);
+    //     this.props.history.push('/recipes');
+    //   })
+    //
+   // }
+
+  render() {
+    console.log('login rendered');
+    return (
+      <div className="login">
+        <form onSubmit={(event) => this.props.handleSubmit(event, this.state)}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={this.handleChange}
+            value={this.state.username}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={this.handleChange}
+            value={this.state.password}
+          />
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Login;
